e5dbf920a6c0099a61805caba01cacd97e140aea
Mozzammel Ridoy <mozzammelridoy5iii@gmail.com>
2024-05-29 16:53:33 +0600

    client side CRUD or project Complete

--- index.js
+++ index.js
@@ -6,12 +6,26 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express(); 
 const port = process.env.PORT || 5000; 
 
+
+const corsOptions = {
+  origin: [
+      'http://localhost:5173',
+      'https://car-and-you-67054.web.app'
+      
+  ],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+
+
 //middleware
-app.use(cors()); 
+app.use(cors(corsOptions)); 
 app.use(express.json()); 
 
 
 //MongoDB here
+// const uri = `mongodb://localhost:27017`;
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.zmeeuxc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -33,6 +47,10 @@ async function run() {
     const bannerAndHeadLineDB = carAndYouDBCollections.collection('bannerAndHeader'); 
     const companiesNameAndLogoDB = carAndYouDBCollections.collection('companiesNameAndLogo'); 
     const moreNewCarLaunchingDB = carAndYouDBCollections.collection('moreNewCarLaunching'); 
+    const brandSliderDB = carAndYouDBCollections.collection('brandSlider');
+    const companiesProductDB = carAndYouDBCollections.collection('companiesProduct'); 
+    const siteUsersDB = carAndYouDBCollections.collection('siteUser'); 
+
 
 
 
@@ -141,7 +159,8 @@ async function run() {
       const updateDoc = {
         $set: {
           name : modifyNameAndLogo.name,
-          logo : modifyNameAndLogo.logo
+          logo : modifyNameAndLogo.logo,
+          details: modifyNameAndLogo.details
         }
       }
       const result = await companiesNameAndLogoDB.updateOne(filter, updateDoc, options); 
@@ -201,12 +220,198 @@ async function run() {
       const updateDoc = {
         $set: {
           about : modifyMoreNewCar.about,
-          image : modifyMoreNewCar.image
+          image : modifyMoreNewCar.image,
         }
       }
       const result = await moreNewCarLaunchingDB.updateOne(filter, updateDoc, options); 
       res.send(result);
     })
+
+
+
+    //brand Slider Operation 
+
+    //get for brand slider
+
+    app.get('/brandSlider', async(req, res)=> {
+      const cursor = brandSliderDB.find(); 
+      const result = await cursor.toArray(); 
+      res.send(result);
+    })
+
+
+
+    //post for brand slider
+    app.post('/brandSlider', async(req, res)=> {
+      const newSlider = req.body; 
+      const result = await brandSliderDB.insertOne(newSlider);  
+      res.send(result);
+    })
+
+
+    //delete for brand slider
+    app.delete('/brandSlider/:id', async(req, res)=> {
+      const id = req.params.id; 
+      const filter = {_id: new ObjectId(id)}; 
+      const result = await brandSliderDB.deleteOne(filter); 
+      res.send(result); 
+    })
+
+
+    //update for brand slider
+    //find for update 
+    app.get('/brandSlider/:id', async(req, res)=>{
+      const id = req.params.id;
+      const filter = {_id : new ObjectId(id)}; 
+
+      const result = await brandSliderDB.findOne(filter); 
+      res.send(result); 
+
+      
+    })
+
+    //update brand slider
+    app.put('/brandSlider/:id', async(req, res)=> {
+      const id = req.params.id;
+      const brandSlider = req.body; 
+
+      const filter = {_id: new ObjectId(id)}; 
+      const options = {upsert : true}; 
+      const updateDoc = {
+        $set: {
+          companyName : brandSlider.companyName,
+          slider : brandSlider.slider,
+        }
+      }
+      const result = await brandSliderDB.updateOne(filter, updateDoc, options); 
+      res.send(result);
+    })
+
+
+
+    //Company products or items -----------------------------------------------
+
+    //get for companies Product
+
+    app.get('/companiesProduct', async(req, res)=> {
+      const cursor = companiesProductDB.find(); 
+      const result = await cursor.toArray(); 
+      res.send(result);
+    })
+
+
+
+    //post for companies Product
+    app.post('/companiesProduct', async(req, res)=> {
+      const newProduct = req.body; 
+      const result = await companiesProductDB.insertOne(newProduct);  
+      res.send(result);
+    })
+
+
+    //delete for companies Product
+    app.delete('/companiesProduct/:id', async(req, res)=> {
+      const id = req.params.id; 
+      const filter = {_id: new ObjectId(id)}; 
+      const result = await companiesProductDB.deleteOne(filter); 
+      res.send(result); 
+    })
+
+
+    //update for companies Product
+    //find for update 
+    app.get('/companiesProduct/:id', async(req, res)=>{
+      const id = req.params.id;
+      const filter = {_id : new ObjectId(id)}; 
+
+      const result = await companiesProductDB.findOne(filter); 
+      res.send(result); 
+
+      
+    })
+
+    //update companies Product
+    app.put('/companiesProduct/:id', async(req, res)=> {
+      const id = req.params.id;
+      const companyItem = req.body; 
+
+      const filter = {_id: new ObjectId(id)}; 
+      const options = {upsert : true}; 
+      const updateDoc = {
+        $set: {
+          companyName : companyItem.companyName,
+          image : companyItem.image,
+          description : companyItem.description,
+          moreImages: companyItem.moreImage
+        }
+      }
+      const result = await companiesProductDB.updateOne(filter, updateDoc, options); 
+      res.send(result);
+    })
+    
+
+
+    //user information DB
+
+    //get for user
+    app.get('/users', async(req, res)=> {
+      const cursor = siteUsersDB.find(); 
+      const result = await cursor.toArray(); 
+      res.send(result);
+    })
+    
+
+
+    //post for user 
+    app.post('/users', async(req, res)=> {
+      const newUser = req.body; 
+      const result = await siteUsersDB.insertOne(newUser);  
+      res.send(result);
+    })
+
+    //delete for user 
+    app.delete('/users/:id', async(req, res)=> {
+      const id = req.params.id; 
+      const filter = {_id: new ObjectId(id)}; 
+      const result = await siteUsersDB.deleteOne(filter); 
+      res.send(result); 
+    })
+
+    //update for user 
+    app.put('/users', async(req, res)=> {
+      
+      const createdAt = req.body; 
+      console.log(createdAt);
+      const filter = {createdAt : createdAt.createdAt}; 
+      const options = {upsert : true}; 
+      const updateDoc = {
+        $set: {
+          lastSignInTime : createdAt.lastSignInTime,
+          displayName: createdAt.displayName,
+          email : createdAt.email
+        }
+      }
+      const result = await siteUsersDB.updateOne(filter, updateDoc, options); 
+      res.send(result);
+    })
+
+    //patch for user
+    app.patch('/users', async(req, res)=> {
+      
+      const userLoging = req.body; 
+      const filter = {email : userLoging.email}; 
+      const options = {upsert : true}; 
+      const updateDoc = {
+        $set: {
+          lastSignInTime : userLoging.lastSignInTime,
+         
+        }
+      }
+      const result = await siteUsersDB.updateOne(filter, updateDoc, options); 
+      res.send(result);
+    })
+
+    
 
 
 
